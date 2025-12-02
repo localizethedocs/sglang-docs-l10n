c5945b43ee61e281bcd44a05ab81befa29e9a5e6
ltdrobot <ltdrobot@localizethedocs.org>
2025-12-02 08:31:14 +0000

    Deploying to pages from @ localizethedocs/sglang-docs-l10n@f4ff455d183a8b3d600b11a0e0b5b64d320e1ceb 🚀

---
+++ en-us/latest/_sources/references/learn_more.md
@@ -0,0 +1,8 @@
+# Learn More and Join the Community
+
+- The development roadmap: https://roadmap.sglang.io
+- The latest SGLang features and updates are shared through the [LMSYS blog](https://lmsys.org/blog/)
+- X (formerly Twitter): https://x.com/lmsysorg
+- LinkedIn: https://www.linkedin.com/company/sgl-project/
+- Join Slack: https://slack.sglang.io/
+- More blogs, slides, and videos about SGLang at [https://github.com/sgl-project/sgl-learning-materials](https://github.com/sgl-project/sgl-learning-materials)
---
+++ ltd-config.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",    "Development"],
+    ["stable",    "Stable Release"],
+    ["0.5",       "Release 0.5"],
+    ["0.4",       "Release 0.4"],
+    ["0.3",       "Release 0.3"],
+    ["0.2",       "Release 0.2"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/sglang-docs-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/sglang-docs-l10n"],
+    ["GitCode",   "https://gitcode.com/localizethedocs/sglang-docs-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/sglang-docs-l10n"],
+  ]
+};
